15995a1c622f65d12eb4e088f8785a43e62eead6
徐仔 <kami.x1g@163.com>
2022-12-07 00:31:02 +0800

    ✨ feat: 22/12/7 - 1 新增登陆逻辑

--- manager-be/src/routers/auth/index.js
+++ manager-be/src/routers/auth/index.js
@@ -1,5 +1,7 @@
 const Router = require('@koa/router')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const { getBody, getOne, setSchema } = require('../../utils')
 
 const User = mongoose.model('User')
 
@@ -8,22 +10,47 @@ const router = new Router({
 })
 
 router.post('/register', async ctx => {
-  const { account, password } = ctx.request.body
+  const { account, password } = getBody(ctx)
 
-  const isExist = await User.findOne({ account }).exec()
+  if (!account || !password) {
+    ctx.body = { code: -1, msg: '字段不能为空', data: null }
+    return
+  }
+
+  const isExist = await getOne(User, { account })
   if (isExist) {
     ctx.body = { code: -1, msg: '账号已存在', data: null }
     return
   }
 
-  const user = new User({ account, password })
-  const data = await user.save()
+  const data = await setSchema(User, { account, password })
 
   ctx.body = { code: 0, msg: '注册成功', data }
 })
 
 router.post('/login', async ctx => {
-  ctx.body = 'login'
+  const { account, password } = getBody(ctx)
+
+  if (!account || !password) {
+    ctx.body = { code: -1, msg: '字段不能为空', data: null }
+    return
+  }
+
+  const isExist = await getOne(User, { account })
+
+  // 无该用户或密码错误
+  if (!isExist || isExist.password !== password) {
+    ctx.body = { code: -1, msg: '用户名或密码错误', data: null }
+    return
+  }
+
+  const user = { account: isExist.account, id: isExist._id }
+
+  ctx.body = {
+    code: 0,
+    msg: '登陆成功',
+    data: { user, token: jwt.sign(user, 'ao-manager') }
+  }
 })
 
 module.exports = router
