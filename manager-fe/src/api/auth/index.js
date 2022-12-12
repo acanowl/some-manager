a7561e5db69e13bc23c1b27b7a27a1be4cfd9592
徐仔 <kami.x1g@163.com>
2022-12-12 22:26:10 +0800

    ✨ feat: 22/12/12 新增goods相关接口

--- manager-fe/src/api/auth/index.js
+++ manager-fe/src/api/auth/index.js
@@ -1,15 +1,17 @@
 import { post } from '@/utils/axios.js'
 
+const prefix = '/auth'
+
 /**
  * 注册接口
  * @param {*} params
  * @returns
  */
-export const registerApi = params => post('/auth/register', params)
+export const registerApi = params => post(`${prefix}/register`, params)
 
 /**
  * 登陆接口
  * @param {*} params
  * @returns
  */
-export const loginApi = params => post('/auth/login', params)
+export const loginApi = params => post(`${prefix}/login`, params)
---
+++ manager-fe/src/api/goods/index.js
@@ -0,0 +1,24 @@
+import { post } from '@/utils/axios.js'
+
+const prefix = '/goods'
+
+/**
+ * 新增接口
+ * @param {*} params
+ * @returns
+ */
+export const goodsAddApi = params => post(`${prefix}/add`, params)
+
+/**
+ * 更新接口
+ * @param {*} params
+ * @returns
+ */
+export const goodsUpdateApi = params => post(`${prefix}/update`, params)
+
+/**
+ * 列表接口
+ * @param {*} params
+ * @returns
+ */
+export const goodsListApi = params => get(`${prefix}/list`, params)
